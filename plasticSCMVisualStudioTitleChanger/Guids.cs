8049afd7d98d650dd2bbf6b6367d503140582723
dhernandez <dhernandez@.(none)>
2016-02-02 18:06:07 +0100

    Add initial empty extension project

---
+++ plasticSCMVisualStudioTitleChanger/Guids.cs
@@ -0,0 +1,14 @@
+﻿// Guids.cs
+// MUST match guids.h
+using System;
+
+namespace CodiceSoftware.plasticSCMVisualStudioTitleChanger
+{
+    static class GuidList
+    {
+        public const string guidplasticSCMVisualStudioTitleChangerPkgString = "433f343b-126f-4dc5-baf8-bc12f7ca07d9";
+        public const string guidplasticSCMVisualStudioTitleChangerCmdSetString = "a905dbe0-e0d4-46d7-814d-6753e0c9ce2d";
+
+        public static readonly Guid guidplasticSCMVisualStudioTitleChangerCmdSet = new Guid(guidplasticSCMVisualStudioTitleChangerCmdSetString);
+    };
+}
---
+++ plasticSCMVisualStudioTitleChanger/plasticSCMVisualStudioTitleChangerPackage.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Diagnostics;
+using System.Globalization;
+using System.Runtime.InteropServices;
+using System.ComponentModel.Design;
+using Microsoft.Win32;
+using Microsoft.VisualStudio;
+using Microsoft.VisualStudio.Shell.Interop;
+using Microsoft.VisualStudio.OLE.Interop;
+using Microsoft.VisualStudio.Shell;
+
+namespace CodiceSoftware.plasticSCMVisualStudioTitleChanger
+{
+    [PackageRegistration(UseManagedResourcesOnly = true)]
+
+    [InstalledProductRegistration("#110", "#112", "1.0", IconResourceID = 400)]
+    [Guid(GuidList.guidplasticSCMVisualStudioTitleChangerPkgString)]
+    public sealed class plasticSCMVisualStudioTitleChangerPackage : Package
+    {
+        public plasticSCMVisualStudioTitleChangerPackage()
+        {
+        }
+
+    }
+}
